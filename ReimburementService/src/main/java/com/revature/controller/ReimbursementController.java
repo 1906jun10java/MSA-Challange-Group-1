31e689a3394e5544969ba6a809da07442954d2af
Richard Di Cosmo <rjdicosmo@gmail.com>
2019-08-12 15:10:25 -0400

    modified the reimbursement service

--- ReimburementService/src/main/java/com/revature/controller/ReimbursementController.java
+++ ReimburementService/src/main/java/com/revature/controller/ReimbursementController.java
@@ -1,12 +1,19 @@
 package com.revature.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/reimbursements")
 public class ReimbursementController {
 
-	
+	@RequestMapping(value="/getAll", method=RequestMethod.GET)
+	public ResponseEntity<String> getReimbursements() {
+		ResponseEntity<String> response = new ResponseEntity<>("It works", HttpStatus.OK);
+		return response;
+	}
 	
 }
