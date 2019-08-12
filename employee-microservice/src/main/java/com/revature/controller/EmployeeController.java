0a540b31c378cbac6a77edefeffec0a1c165fd05
LuckyOwlBen <benmalmberg2@gmail.com>
2019-08-12 16:59:32 -0400

    we touched it

--- employee-microservice/src/main/java/com/revature/controller/EmployeeController.java
+++ employee-microservice/src/main/java/com/revature/controller/EmployeeController.java
@@ -2,9 +2,11 @@ package com.revature.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.Employee;
 import com.revature.service.EmployeeService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value="/employee")
 public class EmployeeController {
 	
-	EmployeeService es;
+	private EmployeeService es;
+	
+	@Autowired
+	public EmployeeController(EmployeeService es) {
+		this.es = es;
+	}
 
-	@RequestMapping(value="", method=RequestMethod.GET)
+	@RequestMapping(value="/all", method=RequestMethod.GET)
 	public ResponseEntity<List<Employee>> getAllEmployees(){
 		List<Employee> employees = es.getAllEmployees();
 		return new ResponseEntity<List<Employee>>(employees, new HttpHeaders(), HttpStatus.OK);
