2820ec4828c0b3f3236a76c4c0f30683104f5c5b
Richard Di Cosmo <rjdicosmo@gmail.com>
2019-08-12 16:04:49 -0400

    fixing employee service

--- employee-microservice/src/main/java/com/example/demo/EmployeeMicroserviceApplication.java
+++ employee-microservice/src/main/java/com/example/demo/EmployeeMicroserviceApplication.java
@@ -1,27 +1,12 @@
 package com.example.demo;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-import com.revature.Employee;
-import com.revature.service.EmployeeService;
-
-@RestController
+@EnableEurekaClient
 @SpringBootApplication
 public class EmployeeMicroserviceApplication {
-	
-	EmployeeService es;
-
-public ResponseEntity<List<Employee>> getAllEmployees(){
-	List<Employee> employees = es.getAllEmployees();
-	return new ResponseEntity<List<Employee>>(employees, new HttpHeaders(), HttpStatus.OK);
-}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeMicroserviceApplication.class, args);
