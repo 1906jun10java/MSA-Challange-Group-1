ed4dcb7bb6aa5099c6f3f1cc5a68ecbd7c818925
Richard Di Cosmo <rjdicosmo@gmail.com>
2019-08-12 14:41:37 -0400

    adding reimbursement and eureka server

---
+++ ReimburementService/src/main/java/com/revature/ReimburementServiceApplication.java
@@ -0,0 +1,18 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+public class ReimburementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReimburementServiceApplication.class, args);
+	}
+
+}
