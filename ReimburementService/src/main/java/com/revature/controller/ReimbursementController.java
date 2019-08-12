93e45daa7b0290cf4ed846b9efb429b7308f01db
Richard Di Cosmo <rjdicosmo@gmail.com>
2019-08-12 16:07:36 -0400

    sfuff

--- ReimburementService/src/main/java/com/revature/controller/ReimbursementController.java
+++ ReimburementService/src/main/java/com/revature/controller/ReimbursementController.java
@@ -2,12 +2,14 @@ package com.revature.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
-@RequestMapping("/reimbursements")
+@RequestMapping(value="/reimbursements")
 public class ReimbursementController {
 
 	@RequestMapping(value="/getAll", method=RequestMethod.GET)
